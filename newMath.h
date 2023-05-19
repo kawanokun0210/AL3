598f268accfc65e858826b22329a0a8c3533be87
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-05-19 21:03:16 +0900

    k

--- newMath.h
+++ newMath.h
@@ -2,15 +2,15 @@
 #include <cmath>
 #include <cassert>
 
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	Vector3 result;
+inline Vector3 Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 result{};
 	result.x = v1.x + v2.x;
 	result.y = v1.y + v2.y;
 	result.z = v1.z + v2.z;
 	return result;
 }
 
-Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+inline Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	Matrix4x4 result{};
 	for (int x = 0; x < 4; x++) {
 		for (int y = 0; y < 4; y++) {
@@ -21,7 +21,7 @@ Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
 	return result;
 }
 
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+inline Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 	Matrix4x4 result{};
 	for (int i = 0; i < 4; i++) {
 		for (int y = 0; y < 4; y++) {
@@ -40,7 +40,7 @@ Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 	return result;
 }
 
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+inline Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 	Matrix4x4 result{};
 	for (int i = 0; i < 4; i++) {
 		for (int y = 0; y < 4; y++) {
@@ -56,81 +56,107 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 	return result;
 }
 
-Matrix4x4 MakeRotateXMatrix(float radian) {
-	Matrix4x4 result{};
+inline Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result;
 	result.m[0][0] = 1;
-	result.m[1][1] = std::cos(radian);
-	result.m[1][2] = std::sin(radian);
-	result.m[2][1] = std::sin(-radian);
-	result.m[2][2] = std::cos(radian);
-	result.m[3][3] = 1;
-	return result;
-}
-Matrix4x4 MakeRotateYMatrix(float radian) {
-	Matrix4x4 result{};
-	result.m[0][0] = std::cos(radian);
-	result.m[1][1] = std::sin(-radian);
-	result.m[1][2] = 1;
-	result.m[2][1] = std::sin(radian);
-	result.m[2][2] = std::cos(radian);
-	result.m[3][3] = 1;
-	return result;
-}
-Matrix4x4 MakeRotateZMatrix(float radian) {
-	Matrix4x4 result{};
-	result.m[0][0] = std::cos(radian);
-	result.m[1][1] = std::sin(-radian);
-	result.m[1][2] = std::sin(radian);
-	result.m[2][1] = std::cos(radian);
-	result.m[2][2] = 1;
-	result.m[3][3] = 1;
-	return result;
-}
-
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
-	Matrix4x4 result{};
-	Matrix4x4 rotateXYZMatrix = Multiply(
-	    MakeRotateXMatrix(rotate.x),
-	    Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
-
-	result.m[0][0] = rotateXYZMatrix.m[0][0] * scale.x;
-	result.m[0][1] = rotateXYZMatrix.m[0][1] * scale.x;
-	result.m[0][2] = rotateXYZMatrix.m[0][2] * scale.x;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
 	result.m[0][3] = 0;
 
-	result.m[1][0] = rotateXYZMatrix.m[1][0] * scale.y;
-	result.m[1][1] = rotateXYZMatrix.m[1][1] * scale.y;
-	result.m[1][2] = rotateXYZMatrix.m[1][2] * scale.y;
+	result.m[1][0] = 0;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
 	result.m[1][3] = 0;
 
-	result.m[2][0] = rotateXYZMatrix.m[2][0] * scale.z;
-	result.m[2][1] = rotateXYZMatrix.m[2][1] * scale.z;
-	result.m[2][2] = rotateXYZMatrix.m[2][2] * scale.z;
+	result.m[2][0] = 0;
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
 	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+
+inline Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result;
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = 0;
+	result.m[0][2] = -std::sin(radian);
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = std::sin(radian);
+	result.m[2][1] = 0;
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+
+inline Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result;
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+	return result;
+}
+
+inline Matrix4x4
+    MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+
+	Matrix4x4 rotateMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+	Matrix4x4 result;
+
+	result.m[0][0] = scale.x * rotateMatrix.m[0][0];
+	result.m[0][1] = scale.x * rotateMatrix.m[0][1];
+	result.m[0][2] = scale.x * rotateMatrix.m[0][2];
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = scale.y * rotateMatrix.m[1][0];
+	result.m[1][1] = scale.y * rotateMatrix.m[1][1];
+	result.m[1][2] = scale.y * rotateMatrix.m[1][2];
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = scale.z * rotateMatrix.m[2][0];
+	result.m[2][1] = scale.z * rotateMatrix.m[2][1];
+	result.m[2][2] = scale.z * rotateMatrix.m[2][2];
+	result.m[2][3] = 0.0f;
 
 	result.m[3][0] = translate.x;
 	result.m[3][1] = translate.y;
 	result.m[3][2] = translate.z;
-	result.m[3][3] = 1;
-
-	return result;
-}
-
-Vector3 TransformCord(Vector3& vector, Matrix4x4& matrix) {
-	Vector3 result{};
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] +
-	           1.0f * matrix.m[3][0];
-	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] +
-	           1.0f * matrix.m[3][1];
-	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] +
-	           1.0f * matrix.m[3][2];
-	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] +
-	          1.0f * matrix.m[3][3];
-
-	assert(w != 0.0f);
-	result.x /= w;
-	result.y /= w;
-	result.z /= w;
+	result.m[3][3] = 1.0f;
 
 	return result;
 }
