4bba52458cf98976f9936eb46926420a08a68af4
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-06-23 15:13:20 +0900

    終わり

--- Enemy.cpp
+++ Enemy.cpp
@@ -7,6 +7,17 @@ Enemy::~Enemy() {
 	for (EnemyBullet* bullet : bullets_) {
 		delete bullet;
 	}
+
+	for (TimedCall* timedCall : timedCall_) {
+		delete timedCall;
+	}
+}
+
+void Enemy::Fire() {
+
+	Attack();
+
+	timedCall_.push_back(new TimedCall(std::bind(&Enemy::Fire, this), 60));
 }
 
 void Enemy::Attack() {
@@ -14,18 +25,18 @@ void Enemy::Attack() {
 	timer--;
 
 	if (timer < 0) {
-		// 弾の速度
-		const float kBulletSpeed = -1.0f;
-		Vector3 velocity(0, 0, kBulletSpeed);
-		velocity = TransformNormal(velocity, worldTransform_.matWorld_);
+	  //弾の速度
+	const float kBulletSpeed = -1.0f;
+	Vector3 velocity(0, 0, kBulletSpeed);
+	velocity = TransformNormal(velocity, worldTransform_.matWorld_);
 
-		EnemyBullet* newBullet = new EnemyBullet();
-		newBullet->Initialize(model_, worldTransform_.translation_, velocity);
-		// 弾を登録
-		// bullet_ = newBullet;
-		bullets_.push_back(newBullet);
+	EnemyBullet* newBullet = new EnemyBullet();
+	newBullet->Initialize(model_, worldTransform_.translation_, velocity);
+	// 弾を登録
+	//bullet_ = newBullet;
+	bullets_.push_back(newBullet);
 
-		timer = kShotInterval;
+	timer = kShotInterval;
 
 	}
 }
@@ -57,6 +68,8 @@ void EnemyStateApproah::Update() {
 	Vector3 appSpeed(0, 0, -0.2f);
 	enemy_->SetPosition(appSpeed);
 
+	enemy_->Fire();
+
 	if (enemy_->GetWT().translation_.z < 0.0f) {
 		enemy_->ChangeState(new EnemyStateLeave);
 	}
@@ -65,6 +78,9 @@ void EnemyStateApproah::Update() {
 void EnemyStateLeave::Update() {
 	Vector3 leaveSpeed(-0.2f, 0.2f, 0.2f);
 	enemy_->SetPosition(leaveSpeed);
+
+	enemy_->Fire();
+
 }
 
 void Enemy::Update() {
@@ -80,9 +96,21 @@ void Enemy::Update() {
 
 	state->Update();
 
-	Attack();
+	// Attack();
+
+	timedCall_.remove_if([](TimedCall* timedCall) {
+		if (timedCall->IsFinished()) {
+			delete timedCall;
+			return false;
+		}
+		return true;
+	});
 
 	worldTransform_.UpdateMatrix();
+
+	for (TimedCall* timedCalls : timedCall_) {
+		timedCalls->Update();
+	}
 
 	// 弾更新
 	for (EnemyBullet* bullet : bullets_) {
